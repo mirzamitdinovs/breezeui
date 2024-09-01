9556eaab0459eefa2e2bea1b487120dde9246e86
mirzamitdinovs <sayyodmirzomitdinov@gmail.com>
2024-09-01 07:28:13 +0500

    app file removed  from dist

--- dist/app/layout.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import type { Metadata } from 'next';
-import './globals.css';
-export declare const metadata: Metadata;
-export default function RootLayout({ children, }: Readonly<{
-    children: React.ReactNode;
-}>): React.JSX.Element;
--- dist/app/page.js
+++
@@ -1,7 +0,0 @@
-import Modal from '@/components/Modal';
-import React from 'react';
-export default function Home() {
-    return (React.createElement("main", { className: 'flex min-h-screen flex-col items-center justify-between p-24' },
-        React.createElement(Modal, null,
-            React.createElement("h2", null, "Hello"))));
-}
